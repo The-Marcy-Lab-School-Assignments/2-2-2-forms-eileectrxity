0d4aafbb4c50b38b2afbc57f1041a1844fe621d9
electrxety <er191712@gmail.com>
2024-08-27 18:05:37 -0400

    solved q10: rendered form values

--- src/index.js
+++ src/index.js
@@ -3,9 +3,17 @@
  *
  */
 
-//QUESTION 9: attaching a submit event listener to the form, ensuring the default behavior is prevented
+//QUESTION 9-10: attaching a submit event listener to the form, ensuring the default behavior is prevented. then updating the text content of the #results section with the form values submitted
+// method 1: using FormData API
 const handleSubmit = (e) => {
-  e.preventDefault();
+  e.preventDefault(); //prevent forms default behavior of reloading
+  const form = e.target;
+  const formValues = Object.fromEntries(new FormData(form)); //getting an obj from the form with key-value pairs of the form's name fields and their corresponding input values
+  document.getElementById('results-username').textContent = formValues.username;
+  document.getElementById('results-coding-level').textContent = formValues.codingLevel;
+  document.getElementById('results-location').textContent = formValues.location;
+  document.getElementById('results-did-like-assignment').textContent = formValues.didLikeAssignment ? 'Yes' : 'No'; //converting default 'on' or 'off' checkbox states to 'Yes' or 'No'
+  debugger; //pauses code + enables debugging directly in the dev tools console as one would console.log here
 };
 
 const main = () => { //runner func for readability + maintainability
