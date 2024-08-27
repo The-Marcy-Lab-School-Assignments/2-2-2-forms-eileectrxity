a93b618af15e08074c49b441aeb93605616a4914
electrxety <er191712@gmail.com>
2024-08-27 18:04:44 -0400

    solved q9: attached submit listener

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
+//2.2.2 Assignment: Forms by Eileen
 /** GENERAL FEEDBACK:
  *
  */
 
-const handleSubmit = () => {
+//QUESTION 9: attaching a submit event listener to the form, ensuring the default behavior is prevented
+const handleSubmit = (e) => {
+  e.preventDefault();
 };
+
+const main = () => { //runner func for readability + maintainability
+  const form = document.querySelector('#new-user-form');
+  form.addEventListener('submit', handleSubmit);
+};
+
+main();
